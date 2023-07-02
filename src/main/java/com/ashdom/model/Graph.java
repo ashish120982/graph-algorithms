11bed9cb56b96b33efa5ebf45dd791a7ac46e868
Ashish Sharma <ashish9458@yahoo.com>
2023-07-02 09:36:28 +0100

    DAG hasPath using DFS

--- src/main/java/com/ashdom/model/Graph.java
+++ src/main/java/com/ashdom/model/Graph.java
@@ -2,7 +2,6 @@ package com.ashdom.model;
 
 import java.util.Deque;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 import lombok.Value;
@@ -11,41 +10,61 @@ public class Graph {
 
 	private Map<Node, Set<Edge>> adjacencyMap;
 	private Map<Node, Boolean> visitedMap;
+	private Deque<Node> deque;
 
-	public String depthFirstSearch(Node a, Deque<Node> stack) {
+	public String depthFirstSearch(Node a) {
 		StringBuffer value = new StringBuffer();
-		stack.push(a);
-		while(!stack.isEmpty()) {
-			Node node = stack.pop();
+		deque.push(a);
+		while(!deque.isEmpty()) {
+			Node node = deque.pop();
 			Set<Edge> edgesOfNode = adjacencyMap.get(node);
 			visitedMap.put(node, true);
 			value.append(node.getData()+ " ");
 			edgesOfNode.stream().forEach(edge -> {
 				Node n = edge.getNode2();
 				if (!visitedMap.get(n)) {
-					stack.push(n);
+					deque.push(n);
 				}
 			});
 		}
 		return value.toString().strip();
 	}
 
-	public String breadthFirstSearch(Node a, Queue<Node> queue) {
+	public String breadthFirstSearch(Node a) {
 		StringBuffer sb = new StringBuffer();
-		queue.offer(a);
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
+		deque.offer(a);
+		while(!deque.isEmpty()) {
+			Node node = deque.poll();
 			Set<Edge> edgesOfNode = adjacencyMap.get(node);
 			visitedMap.put(node, true);
 			sb.append(node.getData()+ " ");
 			edgesOfNode.stream().forEach(edge -> {
 				Node n = edge.getNode2();
 				if (!visitedMap.get(n)) {
-					queue.offer(n);
+					deque.offer(n);
 				}
 			});
 		}
 		return sb.toString().strip();
+	}
+
+	public boolean hasPath(Node source, Node destination) {
+		deque.push(source);
+		while(!deque.isEmpty()) {
+			Node node = deque.pop();
+			if (node.equals(destination)) {
+				return true;
+			}
+			Set<Edge> edges = adjacencyMap.get(node);
+			visitedMap.put(node, true);
+			edges.stream().forEach(edge -> {
+				Node n = edge.getNode2();
+				if (!visitedMap.get(n)) {
+					deque.push(n);
+				}
+			});
+		}
+		return false;
 	}
 	
 }
--- src/test/java/com/ashdom/model/GraphTests.java
+++ src/test/java/com/ashdom/model/GraphTests.java
@@ -1,25 +1,27 @@
 package com.ashdom.model;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class GraphTests {
-	
+
 	private Graph graph;
-	private Deque<Node> deque;
 	private Node startNode;
-	
+
 	@BeforeEach
 	void init() {
 		Map<Node, Set<Edge>> adjacencyMap = new LinkedHashMap<>();
@@ -57,18 +59,67 @@ public class GraphTests {
 		adjacencyMap.put(d, edgesOfD);
 		adjacencyMap.put(e, new HashSet<>());
 		adjacencyMap.put(f, new HashSet<>());
-		graph = new Graph(adjacencyMap,visitedMap);
-		deque = new ArrayDeque<>();
+		graph = new Graph(adjacencyMap, visitedMap, new ArrayDeque<>());
 	}
 
 	@Test
 	void testBreadthFirstSearch() {
-		Assertions.assertEquals("a c b e d f", graph.breadthFirstSearch(startNode, deque));
+		Assertions.assertEquals("a c b e d f", graph.breadthFirstSearch(startNode));
 	}
-	
+
 	@Test
 	void testDepthFirstSearch() {
-		Assertions.assertEquals("a b d f c e", graph.depthFirstSearch(startNode, deque));
+		Assertions.assertEquals("a b d f c e", graph.depthFirstSearch(startNode));
 	}
-	
+
+	private static Stream<Arguments> provideNodesToTest() {
+		return Stream.of(Arguments.of(new Node("f"), new Node("k"), true),
+				Arguments.of(new Node("f"), new Node("h"), true), Arguments.of(new Node("j"), new Node("f"), false));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideNodesToTest")
+	void testInputNodesHavePath(Node source, Node destination, boolean expected) {
+		// given
+		Map<Node, Set<Edge>> adjacencyMap = new LinkedHashMap<>();
+		Map<Node, Boolean> visitedMap = new HashMap<>();
+		Node f = new Node("f");
+		visitedMap.put(f, false);
+		Node g = new Node("g");
+		visitedMap.put(g, false);
+		Edge edgeFG = new Edge(f, g, true);
+		Node h = new Node("h");
+		visitedMap.put(h, false);
+		Edge edgeGH = new Edge(g, h, true);
+		Node i = new Node("i");
+		visitedMap.put(i, false);
+		Node j = new Node("j");
+		visitedMap.put(j, false);
+		Edge edgeFI = new Edge(f, i, true);
+		Edge edgeIG = new Edge(i, g, true);
+		Edge edgeJI = new Edge(j, i, true);
+		Node k = new Node("k");
+		visitedMap.put(k, false);
+		Edge edgeIK = new Edge(i, k, true);
+		Set<Edge> edgesOfF = new HashSet<>();
+		edgesOfF.add(edgeFG);
+		edgesOfF.add(edgeFI);
+		adjacencyMap.put(f, edgesOfF);
+		Set<Edge> edgesOfG = new HashSet<>();
+		edgesOfG.add(edgeGH);
+		adjacencyMap.put(g, edgesOfG);
+		Set<Edge> edgesOfH = new HashSet<>();
+		adjacencyMap.put(h, edgesOfH);
+		Set<Edge> edgesOfI = new HashSet<>();
+		edgesOfI.add(edgeIG);
+		edgesOfI.add(edgeIK);
+		adjacencyMap.put(i, edgesOfI);
+		Set<Edge> edgesOfJ = new HashSet<>();
+		edgesOfJ.add(edgeJI);
+		adjacencyMap.put(j, edgesOfJ);
+		adjacencyMap.put(k, new HashSet<>());
+		graph = new Graph(adjacencyMap, visitedMap, new ArrayDeque<>());
+		Assertions.assertEquals(expected, graph.hasPath(source, destination));
+	}
+
 }
